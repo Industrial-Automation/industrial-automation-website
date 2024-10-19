ab84ea875e9b46e061087d37a84eb0ecbf4391b7
Olezhka-web <oleh.protsyshyn.web@gmail.com>
2024-10-19 13:25:43 +0300

    fix: updated styles

--- src/scenes/Cabinet/components/Content/Content.tsx
+++ src/scenes/Cabinet/components/Content/Content.tsx
@@ -2,7 +2,7 @@ import { Projects } from './components/Projects';
 
 export const Content = () => {
   return (
-    <div className='h-full w-full rounded-3xl bg-subtone-black-3'>
+    <div className='h-full w-full rounded-3xl bg-subtone-black-4'>
       <Projects />
     </div>
   );
