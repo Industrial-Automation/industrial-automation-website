19d5796730811381934bbe9076332d11c703e67f
Olezhka-web <oleh.protsyshyn.web@gmail.com>
2024-12-13 15:59:13 +0200

    fix: updated trends

--- src/scenes/Cabinet/components/Content/components/Projects/components/Content/components/Project/components/Trends/Trends.tsx
+++ src/scenes/Cabinet/components/Content/components/Projects/components/Content/components/Project/components/Trends/Trends.tsx
@@ -9,15 +9,19 @@ import {
   Tooltip
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import {
   fetchGetTrendsArchive,
   TrendsArchiveStateType,
   TrendsArchiveType
 } from 'src/reducers/trends-archive';
+import { Input } from 'src/components/Input';
+import { Switcher } from 'src/components/Switcher';
 import { ProjectScreenType } from 'src/reducers/project-screens';
+
+import Translations from './translations';
 
 interface TrendsPropsType {
   projectScreen: ProjectScreenType;
@@ -30,6 +34,22 @@ export const Trends: React.FC<TrendsPropsType> = ({ projectScreen }) => {
     (state) => state.trends_archive
   ) as TrendsArchiveStateType;
 
+  const [dateTimeStart, setDateTimeStart] = useState('');
+  const [dateTimeEnd, setDateTimeEnd] = useState('');
+
+  const [isRealTime, setIsRealTime] = useState(false);
+
+  const formatDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  };
+
   useEffect(() => {
     const fetchTrendsData = async () => {
       await fetchGetTrendsArchive(projectScreen.id);
@@ -37,17 +57,59 @@ export const Trends: React.FC<TrendsPropsType> = ({ projectScreen }) => {
 
     fetchTrendsData();
 
-    const intervalId = setInterval(fetchTrendsData, 2000);
+    const intervalId = setInterval(fetchTrendsData, 3000);
 
     return () => clearInterval(intervalId);
   }, [projectScreen.id]);
 
-  const groupedTrends = Object.values(
-    trends_archive.reduce<Record<string, TrendsArchiveType[]>>((acc, item) => {
-      (acc[item.tag] ||= []).push(item);
+  const groupedTrends = useMemo(
+    () =>
+      Object.values(
+        trends_archive.reduce<Record<string, TrendsArchiveType[]>>((acc, item) => {
+          (acc[item.tag] ||= []).push(item);
 
-      return acc;
-    }, {})
+          return acc;
+        }, {})
+      ),
+    [trends_archive]
+  );
+
+  const sortedGroupedTrends = useMemo(
+    () =>
+      groupedTrends.map((arr) =>
+        arr.sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime())
+      ),
+    [groupedTrends]
+  );
+
+  const filteredTrends = useMemo(
+    () =>
+      sortedGroupedTrends.map((group) => {
+        if (isRealTime) {
+          return group.slice(Math.max(group.length - 10, 0));
+        }
+
+        let filteredGroup = [...group];
+
+        if (dateTimeStart) {
+          filteredGroup = group.filter(
+            (item) =>
+              new Date(new Date(item.created_at).setMilliseconds(0)).getTime() >=
+              new Date(dateTimeStart).getTime()
+          );
+        }
+
+        if (dateTimeEnd) {
+          filteredGroup = filteredGroup.filter(
+            (item) =>
+              new Date(new Date(item.created_at).setMilliseconds(0)).getTime() <=
+              new Date(dateTimeEnd).getTime()
+          );
+        }
+
+        return filteredGroup;
+      }),
+    [dateTimeEnd, dateTimeStart, isRealTime, sortedGroupedTrends]
   );
 
   const getLineOptions = (title: string) => {
@@ -67,7 +129,7 @@ export const Trends: React.FC<TrendsPropsType> = ({ projectScreen }) => {
 
   const getLineData = (data: TrendsArchiveType[]) => {
     return {
-      labels: data.map((item) => item.created_at),
+      labels: data.map((item) => formatDate(new Date(item.created_at))),
       datasets: [
         {
           label: 'Dataset 1',
@@ -81,13 +143,51 @@ export const Trends: React.FC<TrendsPropsType> = ({ projectScreen }) => {
 
   return (
     <div className='scrollbar grid h-full w-full grid-cols-1 gap-5 overflow-y-auto p-2'>
-      {groupedTrends.map((trend, index) => (
-        <Line
-          className='bg-main-white'
-          key={index}
-          options={getLineOptions(trend[0].title)}
-          data={getLineData(trend)}
-        />
+      {filteredTrends.map((trend, index) => (
+        <div key={index} className='mb-10 flex flex-col'>
+          <div className='flex items-center justify-between gap-x-24'>
+            <Switcher
+              labelClassName='text-main-white'
+              size='sm'
+              barColor='gray'
+              checkedBarColor='skyblue'
+              circleColor='white'
+              value={isRealTime}
+              label={Translations.realTimeLabel}
+              onChange={(isChecked) => setIsRealTime(isChecked)}
+            />
+
+            <div className='flex gap-x-5'>
+              <Input
+                className='mb-5 h-8 w-52 bg-main-white shadow-skyblue ring-0 [&>input]:text-sm'
+                labelClassName='text-main-white'
+                type='datetime-local'
+                label={Translations.startLabel}
+                value={dateTimeStart}
+                step={1}
+                onChange={(e) => setDateTimeStart(e.target.value)}
+                disabled={isRealTime}
+              />
+
+              <Input
+                className='mb-5 h-8 w-52 bg-main-white shadow-skyblue ring-0 [&>input]:text-sm'
+                labelClassName='text-main-white'
+                type='datetime-local'
+                label={Translations.endLabel}
+                value={dateTimeEnd}
+                step={1}
+                onChange={(e) => setDateTimeEnd(e.target.value)}
+                disabled={isRealTime}
+              />
+            </div>
+          </div>
+
+          <Line
+            className='bg-main-white'
+            options={getLineOptions(trend[0]?.title || '')}
+            data={getLineData(trend)}
+          />
+        </div>
       ))}
     </div>
   );
